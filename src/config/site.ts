cbaffdf298780a8730db9943cc9d7485a87b6563
Sandro <sancho12martin@gmail.com>
2026-09-18 22:17:59 +0200

    Responsive mobile + site.ts + nouveaux films

---
+++ src/config/site.ts
@@ -0,0 +1,62 @@
+export const site = {
+
+  // ── HERO ──
+  hero: {
+    vimeoId: "1191904895",
+    hauteur: "80vh",
+  },
+
+  // ── RÉSEAUX SOCIAUX ──
+  reseaux: [
+    {
+      nom: "Email",
+      url: "mailto:pablo12martin@gmail.com",
+      actif: true,
+      img: "/images/mail_logo.svg",
+      classe: "mail-img",
+    },
+    {
+      nom: "Instagram",
+      url: "https://www.instagram.com/instouzou/",
+      actif: true,
+      img: "/images/instagram_logo.svg",
+      classe: "",
+    },
+    {
+      nom: "Vimeo",
+      url: "https://vimeo.com/user128877784",
+      actif: true,
+      img: "/images/vimeo_logo.svg",
+      classe: "",
+    },
+    {
+      nom: "YouTube",
+      url: "https://www.youtube.com/@Pablo12Martin",
+      actif: true,
+      img: "/images/youtube_logo.svg",
+      classe: "",
+    },
+    {
+      nom: "Lightyshare",
+      url: "https://www.lightyshare.com/fr/public/15940/show",
+      actif: true,
+      img: "/images/Lightyshare_logo.png",
+      classe: "lighty-img",
+    },
+    {
+      nom: "LinkedIn",
+      url: "https://www.linkedin.com/in/pablo-martin-910780212/",
+      actif: true,
+      img: "/images/linkedin_logo.svg",
+      classe: "",
+    },
+    {
+      nom: "IMDb",
+      url: null,
+      actif: true,
+      img: "/images/IMBd_logo.png",
+      classe: "imdb-img",
+    },
+  ],
+
+};
